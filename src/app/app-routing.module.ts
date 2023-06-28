deb0a9ce0069be295e4e74dab1fa4625bdc47b7a
LoicZHU <zhu.loic@gmail.com>
2023-06-28 14:19:27 +0200

    add navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule, Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then((m) => m.AdminModule) },
+  { path: 'pokemons', loadChildren: () => import('./pokemons/pokemons.module').then((m) => m.PokemonsModule) },
+  { path: '', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+  { path: '**', loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
