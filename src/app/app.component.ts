9c65c99b6ed8847f4f638c45807265badfcfccce
Mazene <mmazenezerguine@gmail.com>
2023-06-28 12:24:06 +0200

    crud

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CoreService } from './core/services/core/core.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private core: CoreService) {}
+
+  ngOnInit(): void {
+    this.core.saveData();
+    console.log(this.core.getAll());
+
+    const eleme = this.core.getOneByField(1);
+
+    this.core.create(eleme);
+
+    console.log(this.core.getAll());
+
+    this.core.delete(24);
+
+    console.log(this.core.getAll());
+  }
   title = 'pokemon-guesser';
 }
--- src/app/core/services/core/core.service.ts
+++ src/app/core/services/core/core.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../../environments/environment';
+import * as fs from 'fs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,42 +11,54 @@ export abstract class CoreService {
   private readonly _apiUrl: string;
 
   protected constructor(protected readonly httpClient: HttpClient) {
-    this._apiUrl = environment.apiUrl;
+    this._apiUrl = '../../../../assets/data/data.json';
   }
 
-  create(path: string, payload: any): Observable<void> {
-    return this.httpClient.post<void>(this.getPath(path), payload);
+  saveData(): void {
+    this.httpClient.get(this.getPath(this._apiUrl)).subscribe((response) => {
+      const jsonData = JSON.stringify(response);
+      localStorage.setItem('jsonData', jsonData);
+    });
   }
 
-  delete(path: string, id: number): Observable<void> {
-    return this.httpClient.delete<void>(this.getPath(path, id));
+  create(payload: any): void {
+    const stroData = localStorage.getItem('jsonData');
+    const json = JSON.parse(String(stroData));
+    json.push(payload);
+    localStorage.setItem('jsonData', JSON.stringify(json));
   }
 
-  //#region     GET methods
-  getAll<T>(path: string): Observable<T> {
-    return this.httpClient.get<T>(this.getPath(path));
+  delete(id: number): void {
+    const stroData = localStorage.getItem('jsonData');
+    const json = JSON.parse(String(stroData));
+
+    const index = json.findIndex((element: any) => element.number == id);
+    json.splice(index, 1);
+
+    localStorage.setItem('jsonData', JSON.stringify(json));
   }
 
-  getOneByField<T>(path: string, field: string): Observable<T> {
-    return this.httpClient.get<T>(this.getPath(path, field));
+  getAll<T>(): any[] {
+    const data = String(localStorage.getItem('jsonData'));
+    const jsonData = JSON.parse(data);
+
+    return jsonData;
   }
 
-  getOneById<T>(path: string, id: number): Observable<T> {
-    return this.httpClient.get<T>(this.getPath(path, id));
+  getOneByField<T>(id: string | number): Observable<any> {
+    const data = this.getAll();
+
+    return data.find((element: any) => element.number == id);
   }
+  updateByField(id: number | string, payload: any): void {
+    const stroData = localStorage.getItem('jsonData');
+    const json = JSON.parse(String(stroData));
+    const index = json.findIndex((element: any) => element.number == id);
 
-  //#endregion  GET methods
+    json[index] = payload;
 
-  //#region     UPDATE methods
-  updateByField(path: string, field: string, payload: any): Observable<void> {
-    return this.httpClient.put<void>(this.getPath(path, field), payload);
+    localStorage.setItem('jsonData', json);
   }
-
-  updateById(path: string, id: number, payload: any): Observable<void> {
-    return this.httpClient.put<void>(this.getPath(path, id), payload);
-  }
-
-  //#endregion  UPDATE methods
 
   protected getPath(_path: string, id?: number | string): string {
     const path = `${this._apiUrl}/${_path}`;
