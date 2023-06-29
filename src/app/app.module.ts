f611cb949a06e59cb6a88b666b0171589a4f95fe
Mazene <mmazenezerguine@gmail.com>
2023-06-29 08:20:57 +0200

    fixing build bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HeaderComponent } from './shared/components/header/header.component';
 import { NgModule, OnInit } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -8,10 +9,9 @@ import { CoreService } from './core/services/core/core.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, NgbModule, HttpClientModule],
-  providers: [HttpClientModule, HttpClient],
-
+  declarations: [AppComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, NgbModule],
+  providers: [],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
